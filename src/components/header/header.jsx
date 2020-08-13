0ce0d5317a95b3ee7c48b072f9b17b01a8aa21f9
burik84-home <avburik84@gmail.com>
2020-08-14 01:00:19 +0300

    init: style from header, add bootstrap

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,40 +1,46 @@
 import React from 'react';
+import { Fragment } from 'react';
 import './header.css'
 
 const Header = () => {
     return (
-      <div className="header d-flex">
-        <h2>
-          <a href="#">
-            Songbird
-          </a>
-        </h2>
-        <div className='list-questions'>
-            <ul className="d-flex">
-            <li>
-                Warm up
-            </li>
-            <li>
-                Passerines
-            </li>
-            <li>
-                Forest birds
-            </li>
-            <li>
-                Song birds
-            </li>
-            <li>
-                Predator birds
-            </li>
-            <li>
-                Sea birds
-            </li>
-            </ul>
+      <>
+        <div className="container header d-flex">
+          <div className='header-content d-flex'>
+            <h2 className='header-title'>
+              <a className='navbar-brand' href="#">
+                Songbird
+              </a>
+            </h2>
+            <div className='game-score'>
+                <span className='game-score_assign'>+ 0</span>
+                <span className='game-score_item'>Score: 00</span>
+            </div>
+          </div>
+          <div className='list-questions'>
+              <ul className="nav nav-tabs list-items">
+                <li className="list-item nav-item">
+                    Warm up
+                </li>
+                <li className="list-item nav-item">
+                    Passerines
+                </li>
+                <li className="list-item nav-item">
+                    Forest birds
+                </li>
+                <li className="list-item nav-item">
+                    Song birds
+                </li>
+                <li className="list-item nav-item">
+                    Predator birds
+                </li>
+                <li className="list-item nav-item">
+                    Sea birds
+                </li>
+              </ul>
+          </div>
         </div>
-        <div className='game-score'>
-            <span className='game-score_item'>00</span>
-        </div>
-      </div>
+      </>
     );
   };
   
