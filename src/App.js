df446e0b8a0032ff7b9609ff9ad9ab56cec56765
burik84-home <avburik84@gmail.com>
2020-08-22 21:23:40 +0300

    feat: add get answer item

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Question from './components/questions/question';
 import Answer from './components/answer/answer';
 import Description from './components/description/description';
 
+import birdsData from './services/birdsData';
+
 export default class App extends Component{
 
 
@@ -19,9 +21,21 @@ export default class App extends Component{
     totalScore:0,
     levelScore:6,
     numberAnswerTrue:2,
-    answerTrue:false,
+    birdData:[
+      ...birdsData[0].map(({name,id })=>{
+        return this.createBirdItem(name,id)
+      })
+    ]
   }
 
+createBirdItem(name,id){
+    return {
+      name:name,
+      answerTrue:false,
+      answerFalse:false,
+      id:id,
+    }
+  }
   // Переход на следующую группу вопросов в headerе
   onChangeLevel=()=>{
     this.setState(()=>{
@@ -32,14 +46,34 @@ export default class App extends Component{
     })
   }
 
-  // Получение номера ответа от компонента Answer
-  getAnswerNumber=(num)=>{
-    this.setState({
-      answerNumber:num,
+  // Получение номера ответа от компонента Answer -  answer item
+   onToggleAnswer=(id)=>{
+     console.log('Item click', id);
+     this.setState(({ birdData })=>{
+      const idx=birdData.findIndex((el)=>el.id===id);
+
+      // 1. update object      
+      const oldItem=birdData[idx];
+      const newItem={...oldItem, 
+        answerTrue:id===this.state.numberAnswerTrue?true:false,
+        answerFalse:id===this.state.numberAnswerTrue?false:true,
+      };
+
+      // 2. construct new array
+      const newArray=[
+        ...birdData.slice(0,idx),
+        newItem,
+        ...birdData.slice(idx+1)
+      ];
+
+      return {
+        birdData:newArray,
+      }
     })
   }
 
   render(){ 
+    const { birdData}=this.state;
     return (      
       <div className="App">
         <Header 
@@ -49,7 +83,7 @@ export default class App extends Component{
         <Question/>
         <section className='container section'>
           <div className='row'>
-            <Answer getAnswerNumber={this.getAnswerNumber}/>
+            <Answer birdData={birdData} onToggleAnswer={this.onToggleAnswer} />
             <Description/>
           </div>         
         </section>      
--- src/components/answer/answer.jsx
+++ src/components/answer/answer.jsx
@@ -1,55 +1,30 @@
 import React from 'react';
-import './answer.css'
+import './answer.css';
 
-import birdsData from '../../services/birdsData';
 class Answer extends React.Component{
-  constructor(props){
-    super(props)
-  }
-
-  state={
-    numberAnswerTrue:2,
-    getAnswer:0,
-    answerTrue:false,
-  }
-
-// получение номера ответа по списку для поднятия наверх в компонент APP и сравнения с номером правильного ответа
-
-  onClickAnswer=(e)=>{
-    const answer=+e.target.getAttribute('data-id');
-    this.props.getAnswerNumber(answer);
-    // console.log(answer, this.state.trueAnswer);
-    // if(this.state.numberAnswerTrue===answer){
-    //   console.log('правильный ответ');
-    //   this.setState({
-    //     getAnswer: true,
-    //     }
-    //   )
-    // }else{
-    //   console.log('ответ не правильный');
-    //   this.setState({
-    //     getAnswer: this.state.getAnswer+1,
-    //     }
-    //   )
-    // }
-  }
 
   render(){
-    const BirdName=birdsData[0].map(({name,id })=>{
+    const {birdData, onToggleAnswer}=this.props;
+
+    const elements=birdData.map((item)=>{
+      const {id, answerFalse, answerTrue, name}=item
+      let classNames='answer-item list-group-item d-flex justify-content-between align-items-center';
+      if(answerTrue){
+          classNames+=' answer-item--true disabled'
+      }
+      if(answerFalse){
+          classNames+=' answer-item--false disabled'
+      }
       return(
-        <li className='answer-item list-group-item d-flex justify-content-between align-items-center ' data-id={id} key={name}>{name} </li>
+        <li key={id} className={classNames} onClick={()=>onToggleAnswer(id)}>
+          {name}
+        </li>        
       )
     })
     return(
       <div className='answer col-md-5 col-sm-4 col-12'>
-        <ul className='answer-items list-group' onClick={this.onClickAnswer}>
-          {/* <li className='answer-item list-group-item d-flex justify-content-between align-items-center '>Eagle </li>
-          <li className='answer-item answer-item--true disabled list-group-item d-flex justify-content-between align-items-center'>Kite</li>
-          <li className='answer-item list-group-item d-flex justify-content-between align-items-center'>Harrier</li>
-          <li className='answer-item answer-item--false disabled list-group-item d-flex justify-content-between align-items-center'>Falcon</li>
-          <li className='answer-item answer-item--false disabled list-group-item d-flex justify-content-between align-items-center'>Hawk</li>
-          <li className='answer-item list-group-item d-flex justify-content-between align-items-center'>Owl</li> */}
-          {BirdName}
+        <ul className='answer-items list-group'>
+          {elements}
         </ul>
       </div>
     )
