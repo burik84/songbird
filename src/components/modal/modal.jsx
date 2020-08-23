661ed2d6297088c297ae5ae5b065f9b723884a5b
burik84-home <avburik84@gmail.com>
2020-08-23 16:58:12 +0300

    feat: modal add style, class

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -14,25 +14,28 @@ export default class Modal extends Component{
             classNames='modal';
         }
         return(
-            <div className={classNames}>
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Congrats!</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={()=>closeModal()}>
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div className="modal-body">
-                    <p>You scored {totalScore} of 36 score</p>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-primary" onClick={()=>startAgain()}>Start again</button>
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={()=>closeModal()}>Close</button>
-                </div>
-                </div>
-            </div>
-        </div> 
+            <>
+                <div className={getGameFinal?'overlay':null}></div>
+                <div className={classNames}>
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Congrats!</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={()=>closeModal()}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <p>You scored {totalScore} of 36 score</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" onClick={()=>startAgain()}>Start again</button>
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={()=>closeModal()}>Close</button>
+                        </div>
+                        </div>
+                    </div>
+                </div> 
+            </>            
         )
     }
 }
