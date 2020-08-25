c91d1c364455ab5ae1bebe26d7f298e42d0c89a5
burikAV <AVburik84@gmail.com>
2020-08-25 17:55:01 +0300

    fix: description hiding blocks image, title, audio

--- src/components/description/description.jsx
+++ src/components/description/description.jsx
@@ -15,6 +15,10 @@ export default class Description extends Component{
     let srcImageBird='https://picsum.photos/id/38/150/100?grayscale';
     let srcAudioBird='';
 
+    let classNamesImg='description__image ';
+    let classNamesAudio='audio col-11';
+    let classNamesDiv='description-title col-md-8 col-sm-6 col-6';
+
     if (item!==null) {
       const { name, image, species, description, audio }=birdData[item-1];
       nameBird=name;
@@ -22,19 +26,23 @@ export default class Description extends Component{
       descriptionBird=description;
       srcImageBird=image;
       srcAudioBird=audio;
+  }else{
+    classNamesImg+=' description--disabled';
+    classNamesAudio+=' description--disabled';
+    classNamesDiv+=' description--disabled';
   }
 
     return(
       <div className='description col-md-7 col-sm-8 col-12'>
-        <div className="description__image">
+        <div className={classNamesImg}>
           <img className='image description--image' src={srcImageBird} alt={`Bird ${nameBird}`} ></img>
         </div>
-        <div className='description-title col-md-8 col-sm-6 col-6'>
+        <div className={classNamesDiv}>
           <h4 className='bird-name description-title__name'>{nameBird}</h4>
           <h5 className='bird-name description-title__name-latin'>{speciesBird} </h5>
         </div>  
-        <AudioBlock srcAudio={srcAudioBird} classNames='audio col-12' />  
-        <div className="description__bird col-12">
+        <AudioBlock srcAudio={srcAudioBird} classNames={classNamesAudio} />  
+        <div className="description__bird col-11">
           <p>{descriptionBird} </p>
         </div>
       </div>
